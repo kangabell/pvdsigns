78def23d71341c1a0c280060d651a2d5a7f43956
kangabell <k@kangabell.co>
2019-02-20 15:34:14 -0500

    fallback featured image

--- archive.php
+++ archive.php
@@ -3,16 +3,7 @@
 <?php
 if (have_posts()) :
 	while (have_posts()) : the_post();
-
-		if ( has_post_thumbnail() ) :
-			get_template_part('partials/tile');
-		else :
-		?>
-			<a href="<?php echo get_permalink(); ?>">
-				<?php the_excerpt(); ?>
-			</a>
-		<?php
-		endif;
+		get_template_part('partials/tile');
 	endwhile;
 	the_posts_navigation();
 else :
--- partials/tile.php
+++ partials/tile.php
@@ -1,3 +1,11 @@
 <a class="thumbnail" href="<?php echo get_permalink(); ?>">
-	<?php the_post_thumbnail(); ?>	
+	<?php
+	if ( has_post_thumbnail() ) :
+		the_post_thumbnail();
+	else :
+	?>
+		<img src="<?php echo get_template_directory_uri() . '/missing.png'; ?>" alt="image missing" />
+	<?php
+	endif;
+	?>
 </a>
