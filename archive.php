3ba75112a67ba8319faed7b7a42911fa80852386
kangabell <k@kangabell.co>
2019-02-20 14:24:23 -0500

    fall back to excerpt on older posts without a featured image set

--- archive.php
+++ archive.php
@@ -2,7 +2,17 @@
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-	<?php get_template_part('partials/tile'); ?>
+	<?php
+	if ( has_post_thumbnail() ) :
+		get_template_part('partials/tile');
+	else :
+	?>
+		<a href="<?php echo get_permalink(); ?>">
+			<?php the_excerpt(); ?>
+		</a>
+	<?php
+	endif;
+	?>
 
 <?php endwhile; endif; ?>
 		  
