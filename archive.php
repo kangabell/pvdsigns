295da9b43e0e77aa4fdae395a69fb9c14b831660
kangabell <k@kangabell.co>
2019-02-20 15:15:56 -0500

    add pagination

--- archive.php
+++ archive.php
@@ -1,19 +1,23 @@
 <?php get_header(); ?>
 
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<?php
+if (have_posts()) :
+	while (have_posts()) : the_post();
 
-	<?php
-	if ( has_post_thumbnail() ) :
-		get_template_part('partials/tile');
-	else :
-	?>
-		<a href="<?php echo get_permalink(); ?>">
-			<?php the_excerpt(); ?>
-		</a>
-	<?php
-	endif;
-	?>
-
-<?php endwhile; endif; ?>
+		if ( has_post_thumbnail() ) :
+			get_template_part('partials/tile');
+		else :
+		?>
+			<a href="<?php echo get_permalink(); ?>">
+				<?php the_excerpt(); ?>
+			</a>
+		<?php
+		endif;
+	endwhile;
+	the_posts_navigation();
+else :
+	echo 'Sorry, no posts found.';
+endif;
+?>
 		  
 <?php get_footer(); ?>
