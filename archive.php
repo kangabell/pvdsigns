ffb0d991c87e73946a8fdfd4dd7618f1d6efacaf
kangabell <k@kangabell.co>
2019-02-22 14:29:24 -0500

    add page header to archive pages

--- archive.php
+++ archive.php
@@ -2,6 +2,8 @@
 
 <?php
 if (have_posts()) :
+	the_archive_title( '<h1 class="page-title">', '</h1>' );
+	the_archive_description( '<div class="archive-description">', '</div>' );
 	while (have_posts()) : the_post();
 		get_template_part('partials/tile');
 	endwhile;
