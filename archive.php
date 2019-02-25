7afee7ab9b1073b0591781a343d8b8609b0c0382
kangabell <k@kangabell.co>
2019-02-25 15:31:30 -0500

    adjust Archive pages for mobile

--- archive.php
+++ archive.php
@@ -2,12 +2,18 @@
 
 <?php
 if (have_posts()) :
-	the_archive_title( '<h1 class="page-title">', '</h1>' );
-	the_archive_description( '<div class="archive-description">', '</div>' );
+?>
+	<div class="page-header">
+		<h1 class="page-title"><?php the_archive_title(); ?></h1>
+		<div class="archive-description"><?php the_archive_description();?></div>
+	</div>
+	<div class="flex">
+	<?php
 	while (have_posts()) : the_post();
 		get_template_part('partials/tile');
 	endwhile;
-?>
+	?>
+	</div>
 	<div class="pagination">
 		<?php posts_nav_link(' ','&#8592; Older Posts', 'Newer Posts &#8594;'); ?>
 	</div>
