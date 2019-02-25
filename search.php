0009731a5cfe3dc6c25d1ceea842d140a9e39f25
kangabell <k@kangabell.co>
2019-02-25 15:35:01 -0500

    update search page

--- search.php
+++ search.php
@@ -3,14 +3,22 @@
 <?php
 if (have_posts()) :
 ?>
-	<h1 class="page-title">
-		<?php printf( esc_html__( 'Search Results for: %s', '_s' ), '<span>' . get_search_query() . '</span>' ); ?>
-	</h1>
-<?php
+	<div class="page-header">
+		<h1 class="page-title">
+			<?php printf( esc_html__( 'Search Results for: %s', '_s' ), '<span>' . get_search_query() . '</span>' ); ?>
+		</h1>
+	</div>
+	<div class="flex">
+	<?php
 	while (have_posts()) : the_post();
 		get_template_part('partials/tile');
 	endwhile;
-	the_posts_navigation();
+	?>
+	</div>
+	<div class="pagination">
+		<?php posts_nav_link(' ','&#8592; Older Posts', 'Newer Posts &#8594;'); ?>
+	</div>
+<?php
 else :
 	echo 'Sorry, no posts found.';
 endif;
