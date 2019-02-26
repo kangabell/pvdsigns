ce972c9ba3d0b460d34100685de8c29c10737b66
kangabell <k@kangabell.co>
2019-02-26 14:18:09 -0500

    add alt attribute to thumbnails

--- partials/tile.php
+++ partials/tile.php
@@ -1,7 +1,7 @@
 <a class="thumbnail" href="<?php echo get_permalink(); ?>">
 	<?php
 	if ( has_post_thumbnail() ) :
-		the_post_thumbnail();
+		the_post_thumbnail( 'post-thumbnail', array( 'alt' => get_the_title() ) );
 	else :
 	?>
 		<img src="<?php echo get_template_directory_uri() . '/missing.png'; ?>" alt="image missing" />
