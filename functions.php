19df6600e69e2f63177de8aa7b3b20f944d5ebf2
kangabell <k@kangabell.co>
2019-03-07 15:02:39 -0500

    allow 'related posts' to be bidirectional so we can link between posts easily

--- functions.php
+++ functions.php
@@ -109,6 +109,73 @@ if ( ! function_exists( 'pvdsigns_setup' ) ) :
 
 		add_action( 'init', 'pvdsigns_neighborhood_taxonomy', 0 );
 
+
+		/**
+		 * Make ACF 'Related Items' field bidirectional
+		 *
+		 * @link https://www.advancedcustomfields.com/resources/bidirectional-relationships
+		 */
+
+		function pvdsigns_acf_bidirectional( $value, $post_id, $field  ) {
+
+			$field_name = $field['name'];
+			$field_key = $field['key'];
+			$global_name = 'is_updating_' . $field_name;
+
+			if( !empty($GLOBALS[ $global_name ]) ) return $value;
+
+			$GLOBALS[ $global_name ] = 1;
+
+			if( is_array($value) ) {
+
+				foreach( $value as $post_id2 ) {
+
+					$value2 = get_field($field_name, $post_id2, false);
+
+					if( empty($value2) ) {
+						$value2 = array();
+					}
+
+					if( in_array($post_id, $value2) ) continue;
+
+					$value2[] = $post_id;
+
+					update_field($field_key, $value2, $post_id2);
+
+				}
+
+			}
+
+			$old_value = get_field($field_name, $post_id, false);
+
+			if( is_array($old_value) ) {
+
+				foreach( $old_value as $post_id2 ) {
+
+					if( is_array($value) && in_array($post_id2, $value) ) continue;
+
+					$value2 = get_field($field_name, $post_id2, false);
+
+					if( empty($value2) ) continue;
+
+					$pos = array_search($post_id, $value2);
+
+					unset( $value2[ $pos] );
+
+					update_field($field_key, $value2, $post_id2);
+
+				}
+
+			}
+
+			$GLOBALS[ $global_name ] = 0;
+
+			return $value;
+
+		}
+
+		add_filter('acf/update_value/name=related_items', 'pvdsigns_acf_bidirectional', 10, 3);
+
 	}
 
 endif;
